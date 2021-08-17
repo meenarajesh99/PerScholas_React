be8512fbc28033db23308f392bcfd63ee180acd6
unknown <meenarajesh99@icloud.com>
2021-08-17 12:54:56 -0400

    Compiled typescript file

---
+++ index.js
@@ -0,0 +1,5 @@
+function message(name) {
+    return 'hello, world! My name is ' + name;
+}
+var myName = message('Meena');
+console.log(myName);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function message(name){
+    return 'hello, world! My name is ' + name;
+}
+
+var myName = message('Meena');
+console.log(myName);
